467a03dca245a47f3acedf0eb30c7c9c65e76cca
Adarsh kumar jain <adarshjain583@gmail.com>
2017-11-01 07:18:41 +0000

    Fixes

---
+++ header_1.php
@@ -0,0 +1,71 @@
+<header style="width: 980px; position: absolute; top: 0px; height: 203px; left: 0px;" class="hc1" data-state=" " id="SITE_HEADER" >
+
+<div id="SITE_HEADERcenteredContent" class="hc1centeredContent" >
+	<div id="SITE_HEADERinlineContent" class="hc1inlineContent" >
+
+    	<div style="left: 20px; position: absolute; top: 39px; width: 159px; height: 80px;" data-exact-height="80" data-content-padding-horizontal="0" data-content-padding-vertical="0" title="" class="wp1" id="comp-j9b0afp8" >
+    		<a style="cursor: pointer; width: 159px; height: 80px;" href="home.php" target="_self" id="comp-j9b0afp8link" class="wp1link" >
+    			<div id="comp-j9b0afp8img" data-style="" class="wp1img"  style="position: relative; width: 159px; height: 80px;">
+    				<div class="wp1imgpreloader" id="comp-j9b0afp8imgpreloader" ></div>
+    				<img id="comp-j9b0afp8imgimage" alt="Untitled" data-type="image"  style="width: 159px; height: 80px; object-fit: cover;" src="arrivals_files/9fb5ca_2e6cafceba2146f082b20184c86d223bmv2.jpg">
+    			</div>
+    		</a>
+    	</div>
+
+    	<div style="left: 194px; width: 613px; position: absolute; top: 50px;" class="txtNew" id="comp-j9b0443m" >
+    		<p class="font_7" style="text-align:left; line-height:1.46em;">
+    			<span class="color_15"><span>MALAVIYA INTERNATIONAL AIRPORT</span></span>
+    		</p>
+    	</div>
+
+    	<div style="left: 194px; width: 417px; position: absolute; top: 83px;" class="txtNew" id="comp-j9b0a3bh" >
+    		<p class="font_9" style="text-align:left; line-height:1.55em;"><span class="color_15">Reliable. Prompt. Professional.</span></p>
+    	</div>
+
+    	<nav id="comp-j9b0cgpg" data-menuborder-y="0" data-menubtn-border="0" data-ribbon-els="0" data-label-pad="0" data-ribbon-extra="0" data-drophposition="" data-dropalign="left" dir="ltr" style="left: 5px; width: 850px; position: absolute; top: 150px; height: 30px;" class="style-j9b0cgbd" data-state="left notMobile"  data-dropmode="dropDown">
+
+    	<ul style="text-align:left;" aria-label="Site navigation" role="navigation" id="comp-j9b0cgpgitemsContainer" class="style-j9b0cgbditemsContainer" >
+
+    		<li data-direction="ltr" data-listposition="left" data-data-id="dataItem-j9b035ky" class="style-j9b0cgbdrepeaterButton" data-state="menu  idle link notMobile" id="comp-j9b0cgpg0"  data-original-gap-between-text-and-btn="11" style="width: 70px; height: 30px; position: relative; box-sizing: border-box; overflow: visible;" aria-hidden="false">
+    			<a role="button" tabindex="0" aria-haspopup="false" data-listposition="left" href="home.php" target="_self" id="comp-j9b0cgpg0linkElement" class="style-j9b0cgbdrepeaterButtonlinkElement" >
+    			<div class="style-j9b0cgbdrepeaterButton_gapper" ><div style="text-align:left;" id="comp-j9b0cgpg0bg" class="style-j9b0cgbdrepeaterButtonbg" ><p style="text-align: left; line-height: 30px;" id="comp-j9b0cgpg0label" class="style-j9b0cgbdrepeaterButtonlabel" >Home</p></div>
+    			</div>
+    			</a>
+    		</li>
+
+    		<li data-direction="ltr" data-listposition="center" data-data-id="dataItem-j9e3hs8u" class="style-j9b0cgbdrepeaterButton" data-state="menu  idle link notMobile" id="comp-j9b0cgpg1"  data-original-gap-between-text-and-btn="10" style="width: 165px; height: 30px; position: relative; box-sizing: border-box; overflow: visible;" aria-hidden="false">
+    			<a role="button" tabindex="0" aria-haspopup="true" data-listposition="center" href="shops_and_restaurants.php" target="_self" id="comp-j9b0cgpg1linkElement" class="style-j9b0cgbdrepeaterButtonlinkElement" >
+    			<div class="style-j9b0cgbdrepeaterButton_gapper" ><div style="text-align:left;" id="comp-j9b0cgpg1bg" class="style-j9b0cgbdrepeaterButtonbg" ><p style="text-align: left; line-height: 30px;" id="comp-j9b0cgpg1label" class="style-j9b0cgbdrepeaterButtonlabel" >Shops &amp; Restaurants</p></div>
+    			</div>
+    			</a>
+    		</li>
+
+    		<li data-direction="ltr" data-listposition="center" data-data-id="dataItem-j9b0giaz" class="style-j9b0cgbdrepeaterButton" data-state="menu  idle link notMobile" id="comp-j9b0cgpg2"  data-original-gap-between-text-and-btn="11" style="width: 147px; height: 30px; position: relative; box-sizing: border-box; overflow: visible;" aria-hidden="false">
+    			<a role="button" tabindex="0" aria-haspopup="true" data-listposition="center" href="flight_information.php" target="_self" id="comp-j9b0cgpg2linkElement" class="style-j9b0cgbdrepeaterButtonlinkElement" >
+    			<div class="style-j9b0cgbdrepeaterButton_gapper" ><div style="text-align:left;" id="comp-j9b0cgpg2bg" class="style-j9b0cgbdrepeaterButtonbg" ><p style="text-align: left; line-height: 30px;" id="comp-j9b0cgpg2label" class="style-j9b0cgbdrepeaterButtonlabel" >Flight Information</p></div></div>
+    			</a>
+    		</li>
+
+    		<li data-direction="ltr" data-listposition="center" data-data-id="dataItem-j9e139f1" class="style-j9b0cgbdrepeaterButton" data-state="menu  idle link notMobile" id="comp-j9b0cgpg3"  data-original-gap-between-text-and-btn="10" style="width: 191px; height: 30px; position: relative; box-sizing: border-box; overflow: visible;" aria-hidden="false">
+    			<a role="button" tabindex="0" aria-haspopup="true" data-listposition="center" href="transport_and_directions.php" target="_self" id="comp-j9b0cgpg3linkElement" class="style-j9b0cgbdrepeaterButtonlinkElement" ><div class="style-j9b0cgbdrepeaterButton_gapper" >
+    			<div style="text-align:left;" id="comp-j9b0cgpg3bg" class="style-j9b0cgbdrepeaterButtonbg" ><p style="text-align: left; line-height: 30px;" id="comp-j9b0cgpg3label" class="style-j9b0cgbdrepeaterButtonlabel" >Transport and Directions</p></div></div>
+    			</a>
+    		</li>
+
+    	</ul>
+
+    	<div id="comp-j9b0cgpgmoreButton" class="style-j9b0cgbdmoreButton" ></div>
+
+    	<nav style="visibility: hidden; left: 0px; right: auto; bottom: auto;" data-drophposition="" data-dropalign="left" id="comp-j9b0cgpgdropWrapper" class="style-j9b0cgbddropWrapper" >
+    		<ul style="visibility: hidden; left: 0px; right: auto;" id="comp-j9b0cgpgmoreContainer" class="style-j9b0cgbdmoreContainer" ></ul></nav>
+    	</nav>
+
+    	<div style="left: 847px; width: 304px; position: absolute; top: 154px;" class="txtNew" id="comp-j9ctqbfh" >
+    		<p class="font_9"><a>For flight related information - 011-61234567</a></p>
+    	</div>
+
+	</div>
+
+</div>
+
+</header>
